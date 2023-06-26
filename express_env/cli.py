d4dfc0107cf34660d18b348fd094b3763250e473
Damian Świstowski <damian@swistowski.org>
2023-06-26 17:34:25 +0100

    format code

--- express_env/cli.py
+++ express_env/cli.py
@@ -2,8 +2,10 @@ import argparse
 
 
 def main(args=None):
-    parser = argparse.ArgumentParser(description="Express Env CLI",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter, )
+    parser = argparse.ArgumentParser(
+        description="Express Env CLI",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.parse_args(args=args)
 
 
